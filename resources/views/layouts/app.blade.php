da3a542c7055a6819c7acfad42f6b23e66eefe36
Kemhout <computerapplebananadog@gmail.com>
2022-07-18 00:54:19 +0700

    Procfile for Heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel 8 User Roles and Permissions Tutorial') }}</title>
